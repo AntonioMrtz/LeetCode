7c3e1f0d1f210cb00c2cbd228f277491e3792f0c
AntonioMrtz <antoniomartinezfernandez17@gmail.com>
2023-08-30 12:21:01 +0200

    nuevo algoritmo minimun penalty

--- minimun_penalty_for_a_shop.py
+++ minimun_penalty_for_a_shop.py
@@ -1,6 +1,38 @@
 # https://leetcode.com/problems/minimum-penalty-for-a-shop/
 
+
+
 def bestClosingTime(customers: str) -> int:
+
+    min_penalty = 999999999999999999999
+    best_time = 0
+    penalty = 0
+
+    for pos,char in enumerate(customers):
+
+        if char == "Y":
+            penalty+=1
+
+    best_time = -1
+    min_penalty = penalty
+    
+    for pos,char in enumerate(customers):
+
+        if char == "Y":
+            penalty-=1
+
+        if char == "N":
+            penalty+=1
+        
+        if penalty<min_penalty:
+            min_penalty = penalty
+            best_time = pos
+
+    return best_time+1 if best_time!=-1 else 0
+
+    
+
+""" def bestClosingTime(customers: str) -> int:
 
     best_time = 0
     score = 0
@@ -17,10 +49,11 @@ def bestClosingTime(customers: str) -> int:
             best_score=score
 
     
-    return best_time
-
+    return best_time """
 
 """ print(bestClosingTime("YYNY"))
 print(bestClosingTime("NNNNN"))
 print(bestClosingTime("YYYY"))
-print(bestClosingTime("NNNYNN")) """
+print(bestClosingTime("NNNYNN"))
+print(bestClosingTime("NNNYNN"))
+print(bestClosingTime("YN"))"""
